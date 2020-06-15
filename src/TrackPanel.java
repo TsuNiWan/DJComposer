bdf7a8d2c66ff1dfa246cbfc43c4a9a15031b9de
TsuNiWan <tom007880510@gmail.com>
2020-06-15 22:02:42 +0800

    Add MusicMaker GUI

---
+++ src/TrackPanel.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.Color;
+import javax.swing.border.Border;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class TrackPanel extends JLabel{
+    private boolean activate = false;
+
+    public TrackPanel(){
+        super("");
+        setOpaque(true);
+        Border border = BorderFactory.createLineBorder(Color.darkGray, 3);
+
+        // set the border of this component
+        setBackground(Color.lightGray);
+        setBorder(border);
+        setSize(60,60);
+        addMouseListener(new EventListener());
+    }
+
+    private class EventListener implements MouseListener {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            if(activate) {
+                setBackground(Color.lightGray);
+            }else {
+                setBackground(Color.YELLOW);
+            }
+            System.out.println((activate)?"True":"False");
+            activate = !activate;
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+    }
+}
